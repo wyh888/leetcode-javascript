f41cc0b8fbd889b0b08ff5a2f07d58382853e22f
wangyh <yuho_wang@163.com>
2019-07-17 18:04:25 +0800

    fix 696

--- code/696.count-binary-substrings.js
+++ code/696.count-binary-substrings.js
@@ -7,28 +7,71 @@
  * @param {string} s
  * @return {number}
  */
+
+// 问题：RegExp too big
+// var countBinarySubstrings = function(s) {
+//   let r = []
+
+//   let match = (str) => {
+//     let j = str.match(/^(0+|1+)/)[0]
+//     let o = (j[0] ^ 1).toString().repeat(j.length)
+//     let reg = new RegExp(`^(${j}${o})`)
+//     if (reg.test(str)) {
+//       return RegExp.$1
+//     } else {
+//       return ''
+//     }
+//   }
+
+//   for (let i = 0, len = s.length - 1; i < len; i++) {
+//     let sub = match(s.slice(i))
+//     if (sub) {
+//       r.push(sub)
+//     }
+//   }
+
+//   return r.length
+// };
+
+// 问题：运行时间太长，性能不佳
+// var countBinarySubstrings = function(s) {
+//   let count = 0
+
+//   let match = (str) => {
+//     let j = str.match(/^(0+|1+)/)[0]
+//     let o = (j[0] ^ 1).toString().repeat(j.length)
+//     return j + o === str.slice(0, j.length * 2)
+//   }
+
+//   for (let i = 0, len = s.length - 1; i < len; i++) {
+//     let sub = match(s.slice(i))
+//     sub && count++
+//   }
+
+//   return count
+// };
+
 var countBinarySubstrings = function(s) {
-  let r = []
-
-  let match = (str) => {
-    let j = str.match(/^(0+|1+)/)[0]
-    let o = (j[0] ^ 1).toString().repeat(j.length)
-    let reg = new RegExp(`^(${j}${o})`)
-    if (reg.test(str)) {
-      return RegExp.$1
+  if (!s) {
+    return 0
+  }
+  
+  var count = 0
+  var preCount = 0
+  var curCount = 0
+  var p = s.substring(0, 1)
+  for (var digit of s) {
+    if (digit === p) {
+      curCount++
     } else {
-      return ''
+      count += Math.min(preCount, curCount)
+      preCount = curCount
+      curCount = 1
+      p = digit
     }
   }
-
-  for (let i = 0, len = s.length - 1; i < len; i++) {
-    let sub = match(s.slice(i))
-    if (sub) {
-      r.push(sub)
-    }
-  }
-
-  return r.length
+  
+  return count + Math.min(preCount, curCount)
 };
 
 export default countBinarySubstrings
