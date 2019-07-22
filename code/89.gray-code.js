f7378eb19a445ef57b731bad02ccdd6de7c8c2bc
wangyh <yuho_wang@163.com>
2019-07-22 10:24:20 +0800

    fix 89

--- code/89.gray-code.js
+++ code/89.gray-code.js
@@ -7,46 +7,41 @@
  * @param {number} n
  * @return {number[]}
  */
-// var grayCode = function(n) {
-//   if (n === 0) return [0]
-
-//   let unit = [0, 1]
-//   let result = [0, 1]
-//   let createGrayCode = () => {
-//     let tmp = []
-//     for (let i = 0; i < result.length; i++) {
-//       tmp.push(`${unit[0]}${result[i]}`)
-//     }
-//     for (let i = result.length - 1; i >= 0; i--) {
-//       tmp.push(`${unit[1]}${result[i]}`)
-//     }
-//     return tmp
-//   }
-
-//   for (let i = 1; i < n; i++) {
-//     result = createGrayCode()
-//   }
-
-//   return result.map(item => parseInt(item, 2))
-// };
-
 var grayCode = function(n) {
-  if(n === 0) return [0]
-  if(n === 1) return [0, 1]
-  
-  let arr = ["00", "01", "11", "10"]
-  n -= 2
-  
-  while(n > 0) {
-    let arr1 = arr.map(item => "0" + item),
-        arr2 = arr.map(item => "1" + item).reverse()
-    
-    arr = arr1.concat(arr2)
-    n--
+  if (n === 0) return [0]
+
+  let result = [0, 1]
+  let createGrayCode = () => {
+    let arr1 = result.map(item => `0${item}`),
+        arr2 = result.map(item => `1${item}`).reverse()
+
+    return arr1.concat(arr2)
   }
-  
-  return arr.map(item => parseInt(item, 2))
+
+  for (let i = 1; i < n; i++) {
+    result = createGrayCode()
+  }
+
+  return result.map(item => parseInt(item, 2))
 };
+
+// var grayCode = function(n) {
+//   if(n === 0) return [0]
+//   if(n === 1) return [0, 1]
+  
+//   let arr = ["00", "01", "11", "10"]
+//   n -= 2
+  
+//   while(n > 0) {
+//     let arr1 = arr.map(item => "0" + item),
+//         arr2 = arr.map(item => "1" + item).reverse()
+    
+//     arr = arr1.concat(arr2)
+//     n--
+//   }
+  
+//   return arr.map(item => parseInt(item, 2))
+// };
 
 export default grayCode
 
