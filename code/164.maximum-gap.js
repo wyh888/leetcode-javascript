ea04496e93c8de826e3783bcfb28a8bbde43ca0b
wangyh <yuho_wang@163.com>
2019-07-24 23:23:27 +0800

    164.maximum-gap

---
+++ code/164.maximum-gap.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=164 lang=javascript
+ *
+ * [164] Maximum Gap
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+  if (nums.length < 2) return 0
+
+  nums.sort((a,b) => a - b)
+  let max = 0
+  for (let i = 0, tmp; i < nums.length - 1; i++) {
+    tmp = nums[i + 1] - nums[i]
+    if (tmp > max) {
+      max = tmp
+    }
+  }
+  return max
+};
+
+// maximumGap([1, 3, 100])
+// export default maximumGap
+
---
+++ test/164.test.js
@@ -0,0 +1,5 @@
+import maximumGap from '../code/164.maximum-gap'
+
+test('164.maximum-gap', () => {
+  expect(maximumGap([3, 6, 9, 1])).toBe(3)
+})
