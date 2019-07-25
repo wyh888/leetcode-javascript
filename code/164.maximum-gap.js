a413fc22211ec6efe8cfe42eaf43d60f25d3b50b
wangyh <yuho_wang@163.com>
2019-07-25 20:26:32 +0800

    add 164 another

--- code/164.maximum-gap.js
+++ code/164.maximum-gap.js
@@ -7,19 +7,43 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var maximumGap = function(nums) {
+//   if (nums.length < 2) return 0
+
+//   nums.sort((a,b) => a - b)
+//   let max = 0
+//   for (let i = 0, tmp; i < nums.length - 1; i++) {
+//     tmp = nums[i + 1] - nums[i]
+//     if (tmp > max) {
+//       max = tmp
+//     }
+//   }
+//   return max
+// };
+
 var maximumGap = function(nums) {
   if (nums.length < 2) return 0
 
-  nums.sort((a,b) => a - b)
   let max = 0
-  for (let i = 0, tmp; i < nums.length - 1; i++) {
-    tmp = nums[i + 1] - nums[i]
-    if (tmp > max) {
-      max = tmp
+  let len = nums.length - 1
+  let space
+  for (let i = len, tmp; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      tmp = nums[j]
+      if (tmp > nums[j + 1]) {
+        nums[j] = nums[j + 1]
+        nums[j + 1] = tmp
+      }
+    }
+    if (i < len) {
+      space = nums[i + 1] - nums[i]
+      if (space > max) {
+        max = space
+      }
     }
   }
-  return max
-};
+  return Math.max(max, nums[1] - nums[0])
+}
 
 export default maximumGap
 
--- test/164.test.js
+++ test/164.test.js
@@ -1,5 +1,5 @@
 import maximumGap from '../code/164.maximum-gap'
 
 test('164.maximum-gap', () => {
-  expect(maximumGap([3, 6, 9, 1])).toBe(3)
+  expect(maximumGap([100, 3, 2, 1])).toBe(97)
 })
