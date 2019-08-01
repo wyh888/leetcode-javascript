241de757a13169782ad93327d4e36a19dc629534
wangyh <yuho_wang@163.com>
2019-08-01 13:47:18 +0800

    add 215 another

--- code/215.kth-largest-element-in-an-array.js
+++ code/215.kth-largest-element-in-an-array.js
@@ -8,9 +8,24 @@
  * @param {number} k
  * @return {number}
  */
+// var findKthLargest = function(nums, k) {
+//   return nums.sort((a, b) => b - a)[k - 1]
+// };
+
 var findKthLargest = function(nums, k) {
-  return nums.sort((a, b) => b - a)[k - 1]
-};
+  let len = nums.length - 1
+  for (let i = len; i > len - k; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j+1]) {
+        let tmp = nums[j + 1]
+        nums[j+1] = nums[j]
+        nums[j] = tmp
+      }
+    }
+  }
+
+  return nums[len - (k - 1)]
+}
 
 export default findKthLargest
 
--- test/215.test.js
+++ test/215.test.js
@@ -1,5 +1,9 @@
 import findKthLargest from '../code/215.kth-largest-element-in-an-array'
 
-test('215.kth-largest-element-in-an-array', () => {
+test('215.kth-largest-element-in-an-array:1', () => {
+  expect(findKthLargest([3,2,1,5,6,4], 2)).toBe(5)
+})
+
+test('215.kth-largest-element-in-an-array:2', () => {
   expect(findKthLargest([3,2,3,1,2,4,5,5,6], 4)).toBe(4)
 })
