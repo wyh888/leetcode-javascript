7e03268989b7dfa7604c0709063c0687cb787766
wangyh <yuho_wang@163.com>
2019-08-01 11:07:52 +0800

    215.kth-largest-element-in-an-array

---
+++ code/215.kth-largest-element-in-an-array.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=215 lang=javascript
+ *
+ * [215] Kth Largest Element in an Array
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+  return nums.sort((a, b) => b - a)[k - 1]
+};
+
+export default findKthLargest
+
---
+++ test/215.test.js
@@ -0,0 +1,5 @@
+import findKthLargest from '../code/215.kth-largest-element-in-an-array'
+
+test('215.kth-largest-element-in-an-array', () => {
+  expect(findKthLargest([3,2,3,1,2,4,5,5,6], 4)).toBe(4)
+})
