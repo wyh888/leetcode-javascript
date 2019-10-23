14ae09e65bfe3a93efb557d0382fdb66582c2fc1
wangyh <yuho_wang@163.com>
2019-10-23 15:58:27 +0800

    add 84 another

--- code/84.largest-rectangle-in-histogram.js
+++ code/84.largest-rectangle-in-histogram.js
@@ -29,5 +29,30 @@ var largestRectangleArea = function(heights) {
   return maxArea
 };
 // @lc code=end
-export default largestRectangleArea
+
+ /**
+ * 思路2: 优化暴力解法
+ * 时间复杂度: O(n^2)
+ * 空间复杂度: O(1)
+ */
+// @lc code=start
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+  let maxArea = 0
+
+  for (let i = 0; i < heights.length; i++) {
+    let minHeight = Number.MAX_SAFE_INTEGER
+    for (let j = i; j < heights.length; j++) {
+      minHeight = Math.min(minHeight, heights[j])
+      maxArea = Math.max(maxArea, minHeight * (j - i + 1))
+    }
+  }
+  return maxArea
+};
+// @lc code=end
+
+// export default largestRectangleArea
 
