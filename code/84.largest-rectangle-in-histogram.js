e7fab0a8e8a3165871a2342536108b56fb5e5ce4
wangyh <yuho_wang@163.com>
2019-10-23 15:46:30 +0800

    84.largest-rectangle-in-histogram

---
+++ code/84.largest-rectangle-in-histogram.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=84 lang=javascript
+ *
+ * [84] Largest Rectangle in Histogram
+ */
+
+ /**
+ * 思路1: 暴力解法，获取两两相邻的柱子之间形成的矩形面积，该矩形的高为它们之间最矮柱子的高度，宽为它们之间的距离。
+ * 时间复杂度: O(n^3)
+ * 空间复杂度: O(1)
+ */
+// @lc code=start
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+  let maxArea = 0
+
+  for (let i = 0; i < heights.length; i++) {
+    for (let j = i; j < heights.length; j++) {
+      let minHeight = Number.MAX_SAFE_INTEGER
+      for (let k = i; k <= j; k++) {
+        minHeight = Math.min(minHeight, heights[k])
+      }
+      maxArea = Math.max(maxArea, minHeight * (j - i + 1))
+    }
+  }
+  return maxArea
+};
+// @lc code=end
+export default largestRectangleArea
+
---
+++ test/84.test.js
@@ -0,0 +1,5 @@
+import largestRectangleArea from '../code/84.largest-rectangle-in-histogram'
+
+test('84.largest-rectangle-in-histogram', () => {
+  expect(largestRectangleArea([2, 1, 5, 6, 2, 3])).toBe(10)
+})
