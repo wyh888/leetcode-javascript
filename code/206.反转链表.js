74c38d0bac363b855f43aea74c9dadbcde602a7d
wangyh <yuho_wang@163.com>
2021-06-20 21:49:32 +0800

    feat: 206.反转链表

---
+++ code/206.反转链表.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (!head) {
+    return null
+  }
+  let last = null
+  let next = null
+  while (head !== null) {
+    next = head.next
+    head.next = last
+    last = head
+    head = next
+  }
+  return last
+};
+// @lc code=end
+
